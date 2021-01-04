92887a8d3780a5a5d68cb378630a65d2a3a3012e
Philipp Trunschke <trunschk@math.hu-berlin.de>
2021-01-04 20:40:10 +0100

    bugfix

--- create_icons.py
+++ create_icons.py
@@ -14,7 +14,7 @@ for size in [57, 60, 72, 76, 114, 120, 144, 152, 180]:
     outputs_and_sizes.append((output, size))
 
 for output, size in outputs_and_sizes:
-    os.system("inkscape -z -w {size} -h {size} icon.svg -e {output}".format(size=size, output=output))
+    os.system("inkscape --export-filename={output} -w {size} -h {size} icon.svg".format(size=size, output=output))
 
 html += "<link rel=\"mask-icon\" href=\"/mask_icon.svg\" color=\"#ee4c2c\">\n"
 html += "<meta name=\"theme-color\" content=\"#e8e8e8\">\n"
