5f0488d3e93287d9b1404545610513056618fc2c
Philipp Trunschke <trunschk@math.hu-berlin.de>
2021-01-04 20:21:26 +0100

    bugfix

--- create_icons.py
+++ create_icons.py
@@ -4,17 +4,17 @@ html = ""
 outputs_and_sizes = []
 
 for size in [16, 32, 48, 64, 96, 128, 196, 228, 256]:
-    output = f"favicon-{size}x{size}.png"
-    html += f"<link rel=\"icon\" type=\"image/png\" sizes=\"{size}x{size}\" href=\"/{output}\">\n"
+    output = "favicon-{size}x{size}.png".format(size=size)
+    html += "<link rel=\"icon\" type=\"image/png\" sizes=\"{size}x{size}\" href=\"/{output}\">\n".format(size=size, output=output)
     outputs_and_sizes.append((output, size))
 
 for size in [57, 60, 72, 76, 114, 120, 144, 152, 180]:
-    output = f"apple-touch-icon-{size}x{size}.png"
-    html += f"<link rel=\"apple-touch-icon\" sizes=\"{size}x{size}\" href=\"/{output}\">\n"
+    output = "apple-touch-icon-{size}x{size}.png".format(size=size)
+    html += "<link rel=\"apple-touch-icon\" sizes=\"{size}x{size}\" href=\"/{output}\">\n".format(size=size, output=output)
     outputs_and_sizes.append((output, size))
 
 for output, size in outputs_and_sizes:
-    os.system(f"inkscape -z -w {size} -h {size} icon.svg -e {output}")
+    os.system("inkscape -z -w {size} -h {size} icon.svg -e {output}".format(size=size, output=output))
 
 html += "<link rel=\"mask-icon\" href=\"/mask_icon.svg\" color=\"#ee4c2c\">\n"
 html += "<meta name=\"theme-color\" content=\"#e8e8e8\">\n"
